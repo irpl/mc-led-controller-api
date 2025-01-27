a8db0e86af9f0abc9f28a6761d8190484ae63b1c
Phillip Logan <philo3194@gmail.com>
2025-01-27 00:35:58 -0500

    i hate python

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ current_color: Color = Color(rgb=0)
 
 @app.post("/color")
 async def set_color(color: Color):
+  global current_color
   current_color = color
   return "color set to " + str(current_color.rgb)
 
