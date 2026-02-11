a1c9f211dd9a3fe06f8609bb57487f4c0d3209a7
Phillip Logan <philo3194@gmail.com>
2026-02-11 00:35:38 -0500

    simplified

--- app.py
+++ app.py
@@ -3,34 +3,19 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-color_dictionary = {
-  "WHITE": 0xFFFFFF,
-  "BLACK": 0x0,
-  "ORANGE": 0xff6500,
-  "MAGENTA": 0xff00ff,
-  "CYAN": 0x00ffff,
-  "YELLOW": 0xffff00,
-  "LIME": 0xbfff00,
-  "PINK": 0xff748c,
-  "PURPLE": 0x800080,
-  "BLUE": 0x0000ff,
-  "BROWN": 0xa52a2a,
-  "GREEN": 0x00ff00,
-  "RED": 0xff0000
-}
 class Color(BaseModel):
-  color_name: str
+  r: int
+  g: int
+  b: int
 
-current_color: Color = Color(color_name="BLACK")
+current_color: Color = Color(**{"r": 0, "g": 0, "b": 0})
 
-@app.post("/color")
+@app.post("/wool")
 async def set_color(color: Color):
   global current_color
   current_color = color
-  return "color set to " + current_color.color_name
+  return current_color
 
-@app.get("/color")
+@app.get("/wool")
 async def get_color():
-  return { 
-    "rgb": color_dictionary[current_color.color_name]
-  }
+  return current_color
